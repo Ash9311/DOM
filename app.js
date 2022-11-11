cfe45ac4487cc2ebf500260371af009c9d7e9d87
@Ash9311 <ashutoshmulki7@gmail.com>
2022-11-12 01:11:30 +0530

    themes-1

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hello")
+
+function changeTheme(){
+    let head = document.getElementsByTagName('head')[0];
+
+    //select the current link css tag
+    let linkTag =document.getElementById('lightTheme');
+    console.log(linkTag);
+    console.log(head);
+    //remove the css link tag
+    head.removeChild(linkTag);
+    
+    //create new link
+    let newLinkTag = document.createElement('link');
+    console.log(newLinkTag);
+    console.log(document);
+    
+    newLinkTag.setAttribute('id','cssnew');
+    newLinkTag.setAttribute('rel','stylesheet');
+    
+    //add the new href
+    newLinkTag.setAttribute('href','darktheme,css');
+    console.log(newLinkTag);
+    //append it to the head
+    head.appendChild(newLinkTag);
+}
+
